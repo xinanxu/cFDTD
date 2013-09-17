f6d695a94ff2eac3939dee83727e5df3be5ddb9a
Xinan Xu <xx2153@columbia.edu>
2013-09-17 11:11:23 -0400

    fixing the template in field.h

--- include/field.h
+++ include/field.h
@@ -3,6 +3,8 @@
 #include "structure.h"
 #include <type_traits>
 #include <cassert>
+#include <cmath>
+#include <algorithm>
 #include "boundary.h"
 
 ///Cache optimization
@@ -30,9 +32,58 @@ public:
 	float_t* data[4*dim];
 	float_t* mat[4];
 	float_t CD;
-	field(structure<float_t,dim>* s_in,boundary_layer<float_t,dim> b_in);
+	field(structure<float_t,dim>* s_in,boundary_layer<float_t,dim> b_in){
+		s=s_in;                                                                                            
+        b=b_in;                                                                                            
+        material_init();                                                                                   
+        for(int i=0;i<dim;++i){                                                                            
+                data[Ex+4*dim]=new float_t[s->grids_total]();                                              
+                data[Hx+4*dim]=new float_t[s->grids_total]();                                              
+                data[Dx+4*dim]=NULL;                                                                       
+                data[Bx+4*dim]=NULL;                                                                       
+        }                                                                                                  
+        if(s->is_simple())                                                                                 
+                return;     
+	};
 private:
-	void material_init();
+	void material_init(){
+		       if(s->is_simple()){                                                                                
+                mat[CA]=NULL;                                                                              
+                mat[DA]=NULL;                                                                              
+                mat[DB]=NULL;                                                                              
+                mat[CB]=new float_t[s->grids_total]();                                                     
+                switch(dim){                                                                               
+                        case 1:                                                                            
+                                for(unsigned i=0;i < s->grids[X];++i){                                     
+                                        vec<float_t,1> position((i+0.5)*s->interval);
+                                        dielectric<float_t>* mat_in=s->get_material(position);
+                                        mat[CB][i]=1./mat_in->eps;
+                                }
+                                break;
+                        case 2:
+                                for(unsigned j=0;j < s->grids[Y];++j)
+                                for(unsigned i=0;i < s->grids[X];++i){
+                                        vec<float_t,2> position((i+0.5)*s->interval,(j+0.5)*s->interval);
+                                        dielectric<float_t>* mat_in=s->get_material(position);
+                                        mat[CB][i+j*s->grids[X]]=1./mat_in->eps;
+                                }
+                                break;
+                        case 3:
+                                for(unsigned k=0;k < s->grids[Z];++k)
+                                for(unsigned j=0;j < s->grids[Y];++j)
+                                for(unsigned i=0;i < s->grids[X];++i){
+                                        vec<float_t,3> position((i+0.5)*s->interval,(j+0.5)*s->interval,(k+0.5)*s->interval);
+                                        dielectric<float_t>* mat_in=s->get_material(position);
+                                        mat[CB][i+(j+k*s->grids[Y])*s->grids[X]]=1./mat_in->eps;
+                                }
+                                break;
+                        default:
+                                assert(true);
+                }
+                float_t* max_eps_reverse=std::max_element(mat[CB],mat[CB]+s->grids_total);
+                CD=std::sqrt(*max_eps_reverse)/std::sqrt(dim);
+        }
+	};
 };
 }
 
